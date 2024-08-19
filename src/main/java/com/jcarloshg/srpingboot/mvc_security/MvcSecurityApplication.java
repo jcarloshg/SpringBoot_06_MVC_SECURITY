34a5d1bad0e5519bb694518847558417a50e3c14
jcarloshg <carlosj12336@gmail.com>
2024-08-19 14:24:34 -0600

    add files

---
+++ src/main/java/com/jcarloshg/srpingboot/mvc_security/MvcSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.jcarloshg.srpingboot.mvc_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jcarloshg/srpingboot/mvc_security/MvcSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcarloshg.srpingboot.mvc_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
